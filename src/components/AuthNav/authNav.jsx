e4bbbc0ed823f7edf76f148c9ac423c2140e0f14
ptrodion <rdnpetrenko@gmail.com>
2023-10-24 22:21:39 +0100

    3

--- src/components/AuthNav/authNav.jsx
+++ src/components/AuthNav/authNav.jsx
@@ -1,20 +1,15 @@
-import MenuItem from '@mui/material/MenuItem';
 import { NavLink } from 'react-router-dom';
 import css from './AuthNav.module.css';
 
 export const AuthNav = () => {
   return (
-    <>
-      <MenuItem>
-        <NavLink className={css.link} to="/login">
-          Login
-        </NavLink>
-      </MenuItem>
-      <MenuItem>
-        <NavLink className={css.link} to="/register">
-          Register
-        </NavLink>
-      </MenuItem>
-    </>
+    <div>
+      <NavLink className={css.link} to="/register">
+        Register
+      </NavLink>
+      <NavLink className={css.link} to="/login">
+        Log In
+      </NavLink>
+    </div>
   );
 };
